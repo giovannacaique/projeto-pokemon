b0372ad32efbb539b07bcecc60b6d186c93f69e3
Giovanna <giovannadcaique@gmail.com>
2023-09-23 17:16:37 -0300

    ATUALIZAÇÃO DO JS

--- index.js
+++ index.js
@@ -1,31 +1,25 @@
-botao = document.getElementById('botao')
+const botao = document.getElementById('botao');
 
 function acao() {
-    console.log('ACAO')
-}
-
-function acao() {
-
-    let pokemon = document.getElementById('pokemon').value
+    let pokemon = document.getElementById('pokemon').value;
 
     fetch('https://pokeapi.co/api/v2/pokemon/' + pokemon)
         .then(function (response) {
-            return response.json()
-        }).then(function (response) {
-            console.log(response)
-            console.log(response.sprites.front_default)
-            document.getElementById('imagem').src = response.sprites.front_default
-
-            let html_lista = ''
-            for (const hab of response.abilities) {
-                console.log(hab.ability.name)
-                html_lista += '<li class="list-group-item>'+hab.ability.name+'</li>'
-            }
-            console.log(html_lista)
-            document.getElementById('habilidades').innerHTML = html_lista
+            return response.json();
         })
+        .then(function (response) {
+            console.log(response);
+            console.log(response.sprites.front_default);
+            document.getElementById('imagem').src = response.sprites.front_default;
+
+            let html_lista = '';
+            for (const hab of response.abilities) {
+                console.log(hab.ability.name);
+                html_lista += '<li class="list-group-item">' + hab.ability.name + '</li>';
+            }
+            console.log(html_lista);
+            document.getElementById('habilidades').innerHTML = html_lista;
+        });
 }
 
-botao.addEventListener('click', acao)
-
-
+botao.addEventListener('click', acao);
